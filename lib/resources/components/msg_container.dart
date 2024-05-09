1065c9734e1e1fb7f0ae6536f2922689a0b7a7f9
Bikal Chudal <bikalchudal11@gmail.com>
2024-05-09 11:21:20 +0545

    done

--- lib/resources/components/msg_container.dart
+++ lib/resources/components/msg_container.dart
@@ -1,9 +1,24 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:chat_app/resources/constants.dart';
 import 'package:flutter/material.dart';
 
-class MsgContainer extends StatelessWidget {
+class MsgContainer extends StatefulWidget {
   String msg;
   MsgContainer({super.key, required this.msg});
+
+  @override
+  State<MsgContainer> createState() => _MsgContainerState();
+}
+
+class _MsgContainerState extends State<MsgContainer> {
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController.text = widget.msg;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,51 +29,11 @@ class MsgContainer extends StatelessWidget {
         ),
         InkWell(
           onDoubleTap: () {
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return Container(
-                    height: 100,
-                    width: 300,
-                    child: AlertDialog(
-                      title: Text(
-                        "Edit message",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      content: TextField(),
-                      actions: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            TextButton(
-                              style: ButtonStyle(
-                                  foregroundColor:
-                                      MaterialStatePropertyAll(sColor),
-                                  backgroundColor:
-                                      MaterialStatePropertyAll(pColor)),
-                              onPressed: () {},
-                              child: Text("Cancel"),
-                            ),
-                            TextButton(
-                              style: ButtonStyle(
-                                  foregroundColor:
-                                      MaterialStatePropertyAll(sColor),
-                                  backgroundColor:
-                                      MaterialStatePropertyAll(pColor)),
-                              onPressed: () {},
-                              child: Text("Edit"),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                });
+            editDialog(context);
           },
-          onLongPress: () {},
+          onLongPress: () {
+            deleteDialog(context);
+          },
           child: Container(
             alignment: Alignment.centerRight,
             margin: EdgeInsets.symmetric(vertical: 10),
@@ -74,7 +49,7 @@ class MsgContainer extends StatelessWidget {
                 alignment: Alignment.centerRight,
                 child: Text(
                   textAlign: TextAlign.end,
-                  msg,
+                  widget.msg,
                   style: TextStyle(
                     fontSize: 16,
                     color: Colors.white,
@@ -86,5 +61,102 @@ class MsgContainer extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  Future<dynamic> deleteDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) => Container(
+              height: 100,
+              width: 300,
+              child: AlertDialog(
+                title: Text(
+                  "Do you want to delete this message?",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                actions: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        style: ButtonStyle(
+                            foregroundColor: MaterialStatePropertyAll(sColor),
+                            backgroundColor: MaterialStatePropertyAll(pColor)),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("No"),
+                      ),
+                      TextButton(
+                        style: ButtonStyle(
+                            foregroundColor: MaterialStatePropertyAll(sColor),
+                            backgroundColor: MaterialStatePropertyAll(pColor)),
+                        onPressed: () {},
+                        child: Text("Yes"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ));
+  }
+
+  Future<dynamic> editDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Container(
+            height: 100,
+            width: 300,
+            child: AlertDialog(
+              title: Text(
+                "Edit message",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              content: TextField(
+                controller: textEditingController,
+                cursorColor: pColor,
+                decoration: InputDecoration(
+                  hintText: textEditingController.text,
+                  filled: true,
+                  fillColor: textFieldBg,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+              actions: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      style: ButtonStyle(
+                          foregroundColor: MaterialStatePropertyAll(sColor),
+                          backgroundColor: MaterialStatePropertyAll(pColor)),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Cancel"),
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                          foregroundColor: MaterialStatePropertyAll(sColor),
+                          backgroundColor: MaterialStatePropertyAll(pColor)),
+                      onPressed: () {},
+                      child: Text("Edit"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
